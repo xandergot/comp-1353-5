a2bf77310dc863d3dd7d2493982e634cb95cfddf
xandergot <156118420+xandergot@users.noreply.github.com>
2024-04-15 15:42:09 -0600

    Create min1.py

---
+++ min1.py
@@ -0,0 +1,18 @@
+def min1(input_list: list[int])->int:
+    min = input_list[0]  
+    for value in input_list:
+        if min > value :
+            min = value
+    return min
+
+def min2(input_list: list[int])->int:
+    # performs bubble sort on the input
+    n = len(input_list)
+    for i in range(n):
+        for j in range(1, n-i):
+            if input_list[j - 1] > input_list[j]:
+            # swap elements
+                temp = input_list[j - 1]
+                input_list[j - 1] = input_list[j]
+                input_list[j] = temp
+    return input_list[0]
